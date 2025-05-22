200bea128925036b54e562bf2d06b0cf833cc48b
Sviatoslav <djsv91@gmail.com>
2025-05-22 20:31:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+URL = "http://api.weatherapi.com/v1/current.json?"
+
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.environ.get("WEATHER_API_KEY")
+    if not api_key:
+        raise ValueError("WEATHER_API_KEY is not set in environment variables")
+
+    result = requests.get(
+        URL + f"key={api_key}" + f"&q={CITY}"
+    )
+
+    print(result.json())
 
 
 if __name__ == "__main__":
